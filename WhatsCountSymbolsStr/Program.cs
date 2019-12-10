f51dfd73bd7c55b3fdc5a15dde84c47f9f895b0e
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-11 01:02:12 +0300

    Добавьте файлы проекта.

---
+++ WhatsCountSymbolsStr/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WhatsCountSymbolsStr {
+    class Program {
+        static void Main() {
+            string str = Console.ReadLine();
+            // Разбивает строку на массив подстрок для поиска кол-ва слов в строке
+            var colStr = str.Split(" ");
+            Console.WriteLine(str);
+            // Получает кол-во словв строке
+            Console.WriteLine(colStr.Length);
+            Console.ReadKey();
+        }
+    }
+}
